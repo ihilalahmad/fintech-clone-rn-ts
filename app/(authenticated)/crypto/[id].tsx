09111483cf46b28dee7d47de40d82bb67f4624fe
Hilal Ahmad <ihilalahmadd@gmail.com>
2024-05-03 12:37:44 +0500

    Chart implemented in crypto details screen

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -8,20 +8,43 @@ import {
   ScrollView,
   FlatList,
 } from 'react-native';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Stack, useLocalSearchParams } from 'expo-router';
 import { useHeaderHeight } from '@react-navigation/elements';
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
 import { useQuery } from '@tanstack/react-query';
 import { Ionicons } from '@expo/vector-icons';
+import { CartesianChart, Line, useChartPressState } from 'victory-native';
+import { Ticker } from '@/interfaces/crypto';
+import { Circle, useFont } from '@shopify/react-native-skia';
+import { format } from 'date-fns';
+import * as Haptics from 'expo-haptics';
+import Animated, {
+  SharedValue,
+  useAnimatedProps,
+} from 'react-native-reanimated';
+import { TextInput } from 'react-native-gesture-handler';
 
 const categories = ['Overview', 'News', 'Orders', 'Transactions'];
+
+const AnimatedTextInput = Animated.createAnimatedComponent(TextInput);
+
+function ToolTip({ x, y }: { x: SharedValue<number>; y: SharedValue<number> }) {
+  return <Circle cx={x} cy={y} r={8} color={Colors.primary} />;
+}
 
 const Page = () => {
   const { id } = useLocalSearchParams();
   const headerHeight = useHeaderHeight();
   const [activeIndex, setActiveIndex] = useState(0);
+  const font = useFont(require('@/assets/fonts/SpaceMono-Regular.ttf'));
+  const { state, isActive } = useChartPressState({ x: 0, y: { price: 0 } });
+
+  useEffect(() => {
+    console.log('isActive: ', isActive);
+    if (isActive) Haptics.selectionAsync();
+  }, [isActive]);
 
   const { data } = useQuery({
     queryKey: ['info', id],
@@ -29,6 +52,27 @@ const Page = () => {
       const info = await fetch(`/api/info?id=${id}`).then((res) => res.json());
       return info[+id];
     },
+  });
+
+  const { data: tickers } = useQuery({
+    queryKey: ['tickers'],
+    queryFn: async (): Promise<any[]> =>
+      fetch(`/api/tickers`).then((res) => res.json()),
+  });
+
+  const animatedText = useAnimatedProps(() => {
+    return {
+      text: `${state.y.price.value.value.toFixed(2)} €`,
+      defaultValue: '',
+    };
+  });
+
+  const animatedDateText = useAnimatedProps(() => {
+    const date = new Date(state.x.value.value);
+    return {
+      text: `${date.toLocaleDateString()}`,
+      defaultValue: '',
+    };
   });
 
   return (
@@ -135,6 +179,85 @@ const Page = () => {
         renderItem={({ item }) => (
           <>
             {/* TODO: CHART */}
+            <View style={[defaultStyles.block, { height: 500 }]}>
+              {tickers && (
+                <>
+                  {!isActive && (
+                    <View>
+                      <Text
+                        style={{
+                          fontSize: 30,
+                          fontWeight: 'bold',
+                          color: Colors.dark,
+                        }}
+                      >
+                        {tickers[tickers.length - 1].price.toFixed(2)} $
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 18,
+                          color: Colors.gray,
+                        }}
+                      >
+                        Today
+                      </Text>
+                    </View>
+                  )}
+                  {isActive && (
+                    <View>
+                      <AnimatedTextInput
+                        editable={false}
+                        underlineColorAndroid={'transparent'}
+                        style={{
+                          fontSize: 30,
+                          fontWeight: 'bold',
+                          color: Colors.dark,
+                        }}
+                        animatedProps={animatedText}
+                      >
+                        Testing
+                      </AnimatedTextInput>
+                      <AnimatedTextInput
+                        editable={false}
+                        underlineColorAndroid={'transparent'}
+                        style={{ fontSize: 18, color: Colors.gray }}
+                        animatedProps={animatedDateText}
+                      ></AnimatedTextInput>
+                    </View>
+                  )}
+                  <CartesianChart
+                    chartPressState={state}
+                    axisOptions={{
+                      font,
+                      tickCount: 3,
+                      labelOffset: { x: -2, y: 0 },
+                      labelColor: Colors.gray,
+                      formatYLabel: (v) => `${v} $`,
+                      formatXLabel: (ms) => format(new Date(ms), 'MM/yy'),
+                    }}
+                    data={tickers!}
+                    xKey='timestamp'
+                    yKeys={['price']}
+                  >
+                    {({ points }) => (
+                      <>
+                        <Line
+                          points={points.price}
+                          color={Colors.primary}
+                          strokeWidth={3}
+                        />
+                        {isActive && (
+                          <ToolTip
+                            x={state.x.position}
+                            y={state.y.price.position}
+                          />
+                        )}
+                      </>
+                    )}
+                  </CartesianChart>
+                </>
+              )}
+            </View>
             <View style={[defaultStyles.block, { marginTop: 10 }]}>
               <Text style={styles.subtitle}>Overview</Text>
               <Text style={{ color: Colors.gray }}>{data?.description}</Text>
---
+++ app/test.ts
@@ -0,0 +1,6 @@
+interface RootObject {
+  Code: string;
+  Name: string;
+  RelatedCityName: string;
+  RelatedCityCode: string;
+}
